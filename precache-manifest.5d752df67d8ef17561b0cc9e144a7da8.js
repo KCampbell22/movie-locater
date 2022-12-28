1f58a68944c295a882833db9747bd24ef6bb281c
KCampbell22 <ksoup12@gmail.com>
2022-12-28 14:50:31 -0600

    Updates

---
+++ precache-manifest.5d752df67d8ef17561b0cc9e144a7da8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7888af8b76ba43d81571466632b67c5f",
+    "url": "/final/index.html"
+  },
+  {
+    "revision": "a0fd7d3c72f6074a63a2",
+    "url": "/final/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "04d0b26749c49bbd8282",
+    "url": "/final/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a0fd7d3c72f6074a63a2",
+    "url": "/final/static/js/2.017446a3.chunk.js"
+  },
+  {
+    "revision": "a7f79ea22ef71aeae2c2",
+    "url": "/final/static/js/3.84dfe67c.chunk.js"
+  },
+  {
+    "revision": "04d0b26749c49bbd8282",
+    "url": "/final/static/js/main.ca5f6667.chunk.js"
+  },
+  {
+    "revision": "55887d711fdc7b7286d4",
+    "url": "/final/static/js/runtime~main.080bc1f1.js"
+  }
+]);
